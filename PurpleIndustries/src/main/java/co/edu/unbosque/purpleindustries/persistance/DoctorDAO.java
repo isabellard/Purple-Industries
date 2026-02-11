3f1259fd791b43008272477097ee6f47deb49880
Isabella Rodriguez <irodriguezme@unbosque.edu.co>
2026-02-11 08:54:22 -0500

    Update DoctorDAO.java

--- PurpleIndustries/src/main/java/co/edu/unbosque/purpleindustries/persistance/DoctorDAO.java
+++ PurpleIndustries/src/main/java/co/edu/unbosque/purpleindustries/persistance/DoctorDAO.java
@@ -116,7 +116,7 @@ public class DoctorDAO implements OperacionDAO<Doctor> {
 
         for (int i = 0; i < datos.size(); i++) {
             List<String> fila = datos.get(i);
-            if (fila.size() >= 8) {
+            if (fila.size() >= 4) {
                 try {
                     String nombre = fila.get(0);
                     String fechaDeNacimiento = fila.get(1);

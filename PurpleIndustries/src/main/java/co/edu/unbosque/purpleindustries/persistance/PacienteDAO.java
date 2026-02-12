2f24c9d851fbc6948390b2468c9cca4eea8bbc8e
Biblioteca "Juan Roa Vásquez <irodriguezme@unbosque.edu.co>
2026-02-12 15:47:05 -0500

    PdfTiempo

--- PurpleIndustries/src/main/java/co/edu/unbosque/purpleindustries/persistance/PacienteDAO.java
+++ PurpleIndustries/src/main/java/co/edu/unbosque/purpleindustries/persistance/PacienteDAO.java
@@ -9,87 +9,87 @@ import java.util.List;
 
 public class PacienteDAO implements OperacionDAO<Paciente> {
 
-    private ArrayList<Paciente> listaPacientes;
-    private final String TEXT_FILE_NAME = "paciente";
+	private ArrayList<Paciente> listaPacientes;
+	private final String TEXT_FILE_NAME = "paciente";
 
-    public PacienteDAO() {
-        listaPacientes = new ArrayList<>();
-        cargarDesdeArchivo();
-    }
+	public PacienteDAO() {
+		listaPacientes = new ArrayList<>();
+		cargarDesdeArchivo();
+	}
 
-    @Override
-    public void crear(Paciente nuevoDato) {
-        listaPacientes.add(nuevoDato);
-        escribirEnArchivo();
+	@Override
+	public void crear(Paciente nuevoDato) {
+		listaPacientes.add(nuevoDato);
+		escribirEnArchivo();
 
-    }
+	}
 
-    @Override
-    public int eliminar(int index) {
-        if (index < 0 || index >= listaPacientes.size()) {
-            return 0;
-        } else {
-            listaPacientes.remove(index);
-            escribirEnArchivo();
-            return 1;
-        }
-    }
+	@Override
+	public int eliminar(int index) {
+		if (index < 0 || index >= listaPacientes.size()) {
+			return 0;
+		} else {
+			listaPacientes.remove(index);
+			escribirEnArchivo();
+			return 1;
+		}
+	}
 
-    @Override
-    public int eliminar(Paciente datoAEliminar) {
-        if (listaPacientes.remove(datoAEliminar)) {
-            escribirEnArchivo();
-            return 1;
-        } else {
-            return 0;
-        }
-    }
+	@Override
+	public int eliminar(Paciente datoAEliminar) {
+		if (listaPacientes.remove(datoAEliminar)) {
+			escribirEnArchivo();
+			return 1;
+		} else {
+			return 0;
+		}
+	}
 
-    @Override
-    public int actualizar(String documento, Paciente datoAActualizar) {
-        if (documento == null) {
-            return 0;
-        } else {
-            Paciente p = getPacienteById(documento);
-            if (p != null) {
-                p.setNombre(datoAActualizar.getNombre());
-                p.setFechaDeNacimiento(datoAActualizar.getFechaDeNacimiento());
-                p.setDocumento(datoAActualizar.getDocumento());
-                p.setEmail(datoAActualizar.getEmail());
-                p.setAltura(datoAActualizar.getAltura());
-                p.setPeso(datoAActualizar.getPeso());
-                p.setRh(datoAActualizar.getRh());
-            }
+	@Override
+	public int actualizar(String documento, Paciente datoAActualizar) {
+		if (documento == null) {
+			return 0;
+		} else {
+			Paciente p = getPacienteById(documento);
+			if (p != null) {
+				p.setNombre(datoAActualizar.getNombre());
+				p.setFechaDeNacimiento(datoAActualizar.getFechaDeNacimiento());
+				p.setDocumento(datoAActualizar.getDocumento());
+				p.setEmail(datoAActualizar.getEmail());
+				p.setAltura(datoAActualizar.getAltura());
+				p.setPeso(datoAActualizar.getPeso());
+				p.setRh(datoAActualizar.getRh());
+			}
 
-            escribirEnArchivo();
-            return 1;
-        }
-    }
+			escribirEnArchivo();
+			return 1;
+		}
+	}
 
-    @Override
-    public String mostrarTodo() {
-        String salida = "";
-        for (Paciente paciente : listaPacientes) {
-            salida += paciente.toString();
-        }
-        return salida;
-    }
+	@Override
+	public String mostrarTodo() {
+		String salida = "";
+		for (Paciente paciente : listaPacientes) {
+			salida += paciente.toString();
+		}
+		return salida;
+	}
 
-    @Override
-    public int cantidad() {
-        return listaPacientes.size();
-    }
+	@Override
+	public int cantidad() {
+		return listaPacientes.size();
+	}
 
-    @Override
-    public int comprobarPosicion(int index) {
-        if (index < 0 || index >= listaPacientes.size()) {
-            return 0;
-        } else {
-            return 1;
-        }
-    }
+	@Override
+	public int comprobarPosicion(int index) {
+		if (index < 0 || index >= listaPacientes.size()) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
 
-    public Paciente getPacienteById(String documento) {
+	public Paciente getPacienteById(String documento) {
 		for (Paciente p : listaPacientes) {
 			if (p.getDocumento().equals(documento)) {
 				return p;
@@ -98,98 +98,125 @@ public class PacienteDAO implements OperacionDAO<Paciente> {
 		return null;
 	}
 
-    public void modificarDatosMedicos(String documento, int triage, String diagnostico) {
-        Paciente p = getPacienteById(documento);
+	public void modificarDatosMedicos(String documento, int triage, String diagnostico) {
+		Paciente p = getPacienteById(documento);
 
-        if (p != null) {
-            p.setTriage(triage);
-            p.setDiagnostico(diagnostico);
-            escribirEnArchivo();
-        }
+		if (p != null) {
+			p.setTriage(triage);
+			p.setDiagnostico(diagnostico);
+			escribirEnArchivo();
+		}
 
-    }
+	}
 
-    public void escribirEnArchivo() {
-        List<List<Object>> datos = new ArrayList<>();
+	public String modificarTiempoAtencion(String documento, int tiempoAtencion) {
+		Paciente p = getPacienteById(documento);
 
-        for (Paciente paciente : listaPacientes) {
-            List<Object> fila = new ArrayList<>();
+		if (p != null) {
+			if (tiempoAtencion == 1) {
+				p.setTiempoAtencion("Menos de 1 min");
+				escribirEnArchivo();
+				return "Menos de 1 minuto";
+			} else if (tiempoAtencion == 2) {
+				p.setTiempoAtencion("Entre 2 a 5 min");
+				escribirEnArchivo();
+				return "Entre 2 a 5 min";
+			} else if (tiempoAtencion == 3) {
+				p.setTiempoAtencion("Entre 5 a 10 min");
+				escribirEnArchivo();
+				return "Entre 5 a 10 min";
+			} else if (tiempoAtencion == 4) {
+				p.setTiempoAtencion("Entre 10 a 15 min");
+				escribirEnArchivo();
+				return "Entre 10 a 15 min";
+			} else if (tiempoAtencion == 5) {
+				p.setTiempoAtencion("Mayor a 15 min");
+				escribirEnArchivo();
+				return "Mayor a 15 min";
+			}
+		}
+		return null;
 
-            fila.add(paciente.getNombre());
-            fila.add(paciente.getFechaDeNacimiento());
-            fila.add(paciente.getEdad());
-            fila.add(paciente.getSignoZodiacal());
-            fila.add(paciente.getDocumento());
-            fila.add(paciente.getEmail());
+	}
 
-            double metros = paciente.getAltura();
+	public void escribirEnArchivo() {
+		List<List<Object>> datos = new ArrayList<>();
 
-            fila.add(ConversorAltura.metrosTexto(metros));
-            fila.add(ConversorAltura.cmTexto(metros));
-            fila.add(ConversorAltura.dmTexto(metros));
-            fila.add(ConversorAltura.piesTexto(metros));
-            fila.add(ConversorAltura.codosTexto(metros));
+		for (Paciente paciente : listaPacientes) {
+			List<Object> fila = new ArrayList<>();
 
+			fila.add(paciente.getNombre());
+			fila.add(paciente.getFechaDeNacimiento());
+			fila.add(paciente.getEdad());
+			fila.add(paciente.getSignoZodiacal());
+			fila.add(paciente.getDocumento());
+			fila.add(paciente.getEmail());
 
-            fila.add(paciente.getPeso());
-            fila.add(paciente.getRh());
-            fila.add(paciente.getTriage());
-            fila.add(paciente.getDiagnostico());
+			double metros = paciente.getAltura();
 
-            fila.add(paciente.getFechaIngreso() != null
-                    ? paciente.getFechaIngreso().toString()
-                    : LocalDate.now().toString());
+			fila.add(ConversorAltura.metrosTexto(metros));
+			fila.add(ConversorAltura.cmTexto(metros));
+			fila.add(ConversorAltura.dmTexto(metros));
+			fila.add(ConversorAltura.piesTexto(metros));
+			fila.add(ConversorAltura.codosTexto(metros));
 
-            datos.add(fila);
-        }
+			fila.add(paciente.getPeso());
+			fila.add(paciente.getRh());
+			fila.add(paciente.getTriage());
+			fila.add(paciente.getDiagnostico());
 
-        SheetsManager.append(datos, TEXT_FILE_NAME);
-    }
+			fila.add(paciente.getFechaIngreso() != null ? paciente.getFechaIngreso().toString()
+					: LocalDate.now().toString());
+			fila.add(paciente.getTiempoAtencion());
+			datos.add(fila);
+		}
 
+		SheetsManager.append(datos, TEXT_FILE_NAME);
+	}
 
-    public void cargarDesdeArchivo() {
+	public void cargarDesdeArchivo() {
 
-        List<List<String>> datos = SheetsManager.leerDesdeSheets(TEXT_FILE_NAME, "A:P");
+		List<List<String>> datos = SheetsManager.leerDesdeSheets(TEXT_FILE_NAME, "A:P");
 
-        for (int i = 0; i < datos.size(); i++) {
+		for (int i = 0; i < datos.size(); i++) {
 
-            List<String> fila = datos.get(i);
+			List<String> fila = datos.get(i);
 
-            if (fila.size() >= 16) {
+			if (fila.size() >= 17) {
 
-                try {
+				try {
 
-                    String nombre = fila.get(0);
-                    String fechaDeNacimiento = fila.get(1);
-                    int edad = Integer.parseInt(fila.get(2));
-                    String signoZodiaco = fila.get(3);
-                    String documento = fila.get(4);
-                    String email = fila.get(5);
-                    String alturaTexto = fila.get(6)  .replace(" m", "")  .replace(",", ".")  .trim();
-                    double altura = Double.parseDouble(alturaTexto);
-                    String peso = fila.get(11);
-                    String rh = fila.get(12);
-                    int triage = Integer.parseInt(fila.get(13));
-                    String diagnostico = fila.get(14);
+					String nombre = fila.get(0);
+					String fechaDeNacimiento = fila.get(1);
+					int edad = Integer.parseInt(fila.get(2));
+					String signoZodiaco = fila.get(3);
+					String documento = fila.get(4);
+					String email = fila.get(5);
+					String alturaTexto = fila.get(6).replace(" m", "").replace(",", ".").trim();
+					double altura = Double.parseDouble(alturaTexto);
+					String peso = fila.get(11);
+					String rh = fila.get(12);
+					int triage = Integer.parseInt(fila.get(13));
+					String diagnostico = fila.get(14);
 
-                    String fechaIngresoStr = fila.get(15);
-                    LocalDate fechaIngreso = LocalDate.parse(fechaIngresoStr);
+					String fechaIngresoStr = fila.get(15);
+					LocalDate fechaIngreso = LocalDate.parse(fechaIngresoStr);
+					String tiempoAtencion = fila.get(16);
 
-                    listaPacientes.add(
-                            new Paciente( nombre,fechaDeNacimiento, documento,email,altura,peso,rh,triage,diagnostico, fechaIngreso) );
+					listaPacientes.add(new Paciente(nombre, fechaDeNacimiento, documento, email, altura, peso, rh,
+							triage, diagnostico, fechaIngreso, tiempoAtencion));
 
-                } catch (Exception e) {
+				} catch (Exception e) {
 
-                    System.err.println("Error procesando fila " + (i + 1) + ": " + e.getMessage());
+					System.err.println("Error procesando fila " + (i + 1) + ": " + e.getMessage());
 
-                }
-            }
-        }
-    }
+				}
+			}
+		}
+	}
 
-
-    public ArrayList<Paciente> getListaPacientes() {
-    return listaPacientes;
-}
+	public ArrayList<Paciente> getListaPacientes() {
+		return listaPacientes;
+	}
 
 }
